6a675d4e341719efb8e3364f9e50dfa17a55ee12
Dev Kumar <devkr074@gmail.com>
2025-02-24 09:59:35 +0530

    Arithmetic Operations in Java

---
+++ Program_11.java
@@ -0,0 +1,19 @@
+// Arithmetic Operations in Java
+
+import java.util.Scanner;
+
+public class Program_11 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the first number: ");
+    int num1 = sc.nextInt();
+    System.out.print("Enter the second number: ");
+    int num2 = sc.nextInt();
+    System.out.println("Addition: " + (num1 + num2));
+    System.out.println("Subtraction: " + (num1 - num2));
+    System.out.println("Multiplication: " + (num1 * num2));
+    System.out.println("Division: " + (num1 / num2));
+    System.out.println("Modulus: " + (num1 % num2));
+    sc.close();
+  }
+}
