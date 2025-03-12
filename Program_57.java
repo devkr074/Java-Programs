c49c56c8e4c5a072dd4083578f5997bb99c73cf1
Dev Kumar <devkr074@gmail.com>
2025-03-12 22:45:59 +0530

    Sort an Array in Java

---
+++ Program_57.java
@@ -0,0 +1,11 @@
+// Sort an Array in Java
+
+import java.util.Arrays;
+
+public class Program_57 {
+  public static void main(String[] args) {
+    int[] numbers = { 5, 2, 8, 1, 3 };
+    Arrays.sort(numbers);
+    System.out.println("Sorted array: " + Arrays.toString(numbers));
+  }
+}
