66e823e2db631d11ee4df7e5dc056bac907b7999
Dev Kumar <devkr074@gmail.com>
2025-03-15 13:53:09 +0530

    User Input using Scanner Class in Java

--- Program_2.java
+++ Program_2.java
@@ -1,13 +1,13 @@
-// Print Name in Java
+// User Input using Scanner Class in Java
 
 import java.util.Scanner;
 
 public class Program_2 {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter Your Name: ");
-    String name = sc.nextLine();
-    System.out.println("Hi, My name is " + name + ".\nMy Favourite Programming Language is Java.");
-    sc.close();
-  }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name + "!");
+        sc.close();
+    }
 }
