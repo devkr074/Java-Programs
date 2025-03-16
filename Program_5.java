0a568336c3e7afa34e7c9a24c8242e9b93ca2910
Dev Kumar <devkr074@gmail.com>
2025-03-16 13:57:34 +0530

    Explicit Type Casting in Java

--- Program_5.java
+++ Program_5.java
@@ -1,17 +1,10 @@
-// Check Even or Odd in Java
-
-import java.util.Scanner;
+// Explicit Type Casting in Java
 
 public class Program_5 {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter a number: ");
-    int num = sc.nextInt();
-    if (num % 2 == 0) {
-      System.out.println(num + " is even.");
-    } else {
-      System.out.println(num + " is odd.");
-    }
-    sc.close();
+      double doubleValue = 99.99;
+      int integerValue = (int) doubleValue;
+      System.out.println("Double Value: " + doubleValue);
+      System.out.println("Integer Value: " + integerValue);
   }
 }
