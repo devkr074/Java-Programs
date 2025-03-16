6dcb58432d78f9efdfb4701f462b21e12d902f50
Dev Kumar <devkr074@gmail.com>
2025-03-16 13:58:44 +0530

    StringBuilder Class in Java

--- Program_7.java
+++ Program_7.java
@@ -1,14 +1,16 @@
-// Temperature Convertor in Java
-
-import java.util.Scanner;
+// StringBuilder Class in Java
 
 public class Program_7 {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter Temperature in Celsius: ");
-    double celsius = sc.nextDouble();
-    double fahrenheit = (celsius * 9 / 5) + 32;
-    System.out.println("Temperature in Fahrenheit: " + fahrenheit);
-    sc.close();
+      StringBuilder sb = new StringBuilder("Hello");
+      sb.append(" World");
+      System.out.println("After append: " + sb);
+      sb.insert(5, ",");
+      System.out.println("After insert: " + sb);
+      sb.replace(6, 11, "Java");
+      System.out.println("After replace: " + sb);
+      sb.reverse();
+      System.out.println("After reverse: " + sb);
+      System.out.println("Length: " + sb.length());
   }
 }
