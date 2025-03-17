96f80c5853bd68a5381aa7b867a229b2665deee9
Dev Kumar <devkr074@gmail.com>
2025-03-17 18:43:58 +0530

    User Input in Java

--- Program_3.java
+++ Program_3.java
@@ -1,14 +1,26 @@
-// User Input using BufferedReader Class in Java
+// User Input in Java
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Program_3 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter Your Name: ");
-        String name = reader.readLine();
-        System.out.println("Hello " + name + "!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        System.out.print("What is your gpa: ");
+        double gpa = scanner.nextDouble();
+        System.out.print("Are you a student? (true/false): ");
+        boolean isStudent = scanner.nextBoolean();
+        System.out.println("Hello " + name);
+        System.out.println("You are " + age + " years old");
+        System.out.println("Your gpa is: " + gpa);
+        if (isStudent) {
+            System.out.println("You are enrolled in classes");
+        } else {
+            System.out.println("You are NOT enrolled in classes");
+        }
+        scanner.close();
     }
 }
