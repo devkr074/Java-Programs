3b5bf0f8f04edaea9c091ac898880170da7444bd
Dev Kumar <devkr074@gmail.com>
2025-03-18 20:00:38 +0530

    if Statement in Java

--- Program_7.java
+++ Program_7.java
@@ -1,16 +1,40 @@
-// StringBuilder Class in Java
+// if Statement in Java
+
+import java.util.Scanner;
 
 public class Program_7 {
-  public static void main(String[] args) {
-      StringBuilder sb = new StringBuilder("Hello");
-      sb.append(" World");
-      System.out.println("After append: " + sb);
-      sb.insert(5, ",");
-      System.out.println("After insert: " + sb);
-      sb.replace(6, 11, "Java");
-      System.out.println("After replace: " + sb);
-      sb.reverse();
-      System.out.println("After reverse: " + sb);
-      System.out.println("Length: " + sb.length());
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        int age;
+        boolean isStudent;
+        System.out.print("Enter your name: ");
+        name = scanner.nextLine();
+        System.out.print("Enter your age: ");
+        age = scanner.nextInt();
+        System.out.print("Are you a student (true/false): ");
+        isStudent = scanner.nextBoolean();
+        if (name.isEmpty()) {
+            System.out.println("You didn't enter your name! 😡");
+        } else {
+            System.out.println("Hello " + name + "! 😀");
+        }
+        if (age >= 65) {
+            System.out.println("You are a senior! 👴");
+        } else if (age >= 18) {
+            System.out.println("You are an adult! 🧑");
+        } else if (age < 0) {
+            System.out.println("You haven't been born yet! 👼");
+        } else if (age == 0) {
+            System.out.println("You are a baby! 👶");
+        } else {
+            System.out.println("You are a child! 🧒");
+        }
+        if (isStudent) {
+            System.out.println("You are a student! 🏫");
+        } else {
+            System.out.println("You are NOT a student 🏢");
+        }
+        scanner.close();
+    }
 }
