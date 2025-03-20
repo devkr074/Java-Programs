77a9b4ffaa679daef725fcee7e8415c07ce1f8c7
Dev Kumar <devkr074@gmail.com>
2025-03-20 18:09:02 +0530

    Nested if Statement in Java

--- Program_12.java
+++ Program_12.java
@@ -1,20 +1,27 @@
-// Compare Two Numbers in Java
-
-import java.util.Scanner;
+// Nested if Statement in Java
 
 public class Program_12 {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter the first number: ");
-    int num1 = sc.nextInt();
-    System.out.print("Enter the second number: ");
-    int num2 = sc.nextInt();
-    System.out.println("First Number == Second Number: " + (num1 == num2));
-    System.out.println("First Number != Second Number: " + (num1 != num2));
-    System.out.println("First Number > Second Number " + (num1 > num2));
-    System.out.println("First Number < Second Number " + (num1 < num2));
-    System.out.println("First Number >= Second Number: " + (num1 >= num2));
-    System.out.println("First Number <= Second Number: " + (num1 <= num2));
-    sc.close();
-  }
+    public static void main(String[] args) {
+        boolean isStudent = true;
+        boolean isSenior = true;
+        double price = 9.99;
+        if (isStudent) {
+            if (isSenior) {
+                System.out.println("You get a senior discount of 20%");
+                System.out.println("You get a student discount of 10%");
+                price *= 0.7;
+            } else {
+                System.out.println("You get a student discount of 10%");
+                price *= 0.9;
+            }
+        } else {
+            if (isSenior) {
+                System.out.println("You get a senior discount of 20%");
+                price *= 0.8;
+            } else {
+                price *= 1;
+            }
+        }
+        System.out.printf("The price of a ticket is: $%.2f", price);
+    }
 }
