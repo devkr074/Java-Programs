906848e2dc92cb9765bc82d9c6b310b126fcc8a6
Dev Kumar <devkr074@gmail.com>
2025-03-20 18:10:41 +0530

    String Methods in Java

--- Program_13.java
+++ Program_13.java
@@ -1,21 +1,34 @@
-// Logical Operations in Java
-
-import java.util.Scanner;
+// String Methods in Java
 
 public class Program_13 {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the first boolean value (true/false):");
-    boolean b1 = sc.nextBoolean();
-    System.out.println("Enter the second boolean value (true/false):");
-    boolean b2 = sc.nextBoolean();
-    System.out.println("Enter the third boolean value (true/false):");
-    boolean b3 = sc.nextBoolean();
-    System.out.println("First Value && Second Value: " + (b1 && b2));
-    System.out.println("First Value || Second Value: " + (b1 || b2));
-    System.out.println("!First Value: " + (!b1));
-    System.out.println("First Value && Second Value && Third Value: " + (b1 && b2 && b3));
-    System.out.println("First Value || Second Value || Third Value: " + (b1 || b2 || b3));
-    sc.close();
-  }
+    public static void main(String[] args) {
+        String name = "Bro Code";
+        int length = name.length();
+        System.out.println(length);
+        char letter = name.charAt(0);
+        System.out.println(letter);
+        int index = name.indexOf(" ");
+        System.out.println(index);
+        int lastIndex = name.lastIndexOf("o");
+        System.out.println(lastIndex);
+        name = name.toUpperCase();
+        name = name.toLowerCase();
+        name = name.trim();
+        name = name.replace("o", "a");
+        if (name.isEmpty()) {
+            System.out.println("Your name is empty");
+        } else {
+            System.out.println("Hello " + name);
+        }
+        if (name.contains(" ")) {
+            System.out.println("Your name contains a space");
+        } else {
+            System.out.println("Your name DOESN'T contain any spaces");
+        }
+        if (name.equalsIgnoreCase("password")) {
+            System.out.println("Your name can't be password");
+        } else {
+            System.out.println("Hello " + name);
+        }
+    }
 }
