075093d1bf8f0e2f1ab16cb2732509dfc8f671c7
Dev Kumar <devkr074@gmail.com>
2025-03-21 14:34:47 +0530

    Weight Conversion in Java

--- Program_15.java
+++ Program_15.java
@@ -1,16 +1,31 @@
-// Conditional (Ternary) Operator in Java
+// Weight Conversion in Java
 
 import java.util.Scanner;
 
 public class Program_15 {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter the first number: ");
-    int num1 = sc.nextInt();
-    System.out.print("Enter the second number: ");
-    int num2 = sc.nextInt();
-    int max = (num1 > num2) ? num1 : num2;
-    System.out.println("The larger number is: " + max);
-    sc.close();
+    Scanner scanner = new Scanner(System.in);
+    double weight;
+    double newWeight;
+    int choice;
+    System.out.println("Weight Conversion Program");
+    System.out.println("1: Convert lbs to kgs");
+    System.out.println("2: Convert kgs to lbs");
+    System.out.print("Choose an option: ");
+    choice = scanner.nextInt();
+    if (choice == 1) {
+      System.out.print("Enter the weight in lbs: ");
+      weight = scanner.nextDouble();
+      newWeight = weight * 0.453592;
+      System.out.printf("The new weight in kgs is: %.2f", newWeight);
+    } else if (choice == 2) {
+      System.out.print("Enter the weight in kgs: ");
+      weight = scanner.nextDouble();
+      newWeight = weight * 2.20462;
+      System.out.printf("The new weight in lbs is: %.2f", newWeight);
+    } else {
+      System.out.println("That was not a valid choice");
+    }
+    scanner.close();
   }
 }
