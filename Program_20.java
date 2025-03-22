11f8ccaea18270eed5ef9c530235fe276a845b57
Dev Kumar <devkr074@gmail.com>
2025-03-22 15:24:09 +0530

    Logical Operator in Java

--- Program_20.java
+++ Program_20.java
@@ -1,19 +1,17 @@
 // Logical Operator in Java
 
 public class Program_20 {
-  public static void main(String[] args) {
-      double temp = 35;
-      boolean isSunny = true;
-      if(temp <= 30 && temp >= 0 && isSunny){
-          System.out.println("The weather is GOOD 😀");
-          System.out.println("It is SUNNY outside ☀");
-      }
-      else if(temp <= 30 && temp >= 0 && !isSunny){
-          System.out.println("The weather is GOOD 😀");
-          System.out.println("It is CLOUDY outside ☁");
-      }
-      else if(temp > 30 || temp < 0){
-          System.out.println("The weather is BAD 😩");
-      }
-  }
+    public static void main(String[] args) {
+        double temp = 35;
+        boolean isSunny = true;
+        if (temp <= 30 && temp >= 0 && isSunny) {
+            System.out.println("The weather is GOOD 😀");
+            System.out.println("It is SUNNY outside ☀");
+        } else if (temp <= 30 && temp >= 0 && !isSunny) {
+            System.out.println("The weather is GOOD 😀");
+            System.out.println("It is CLOUDY outside ☁");
+        } else if (temp > 30 || temp < 0) {
+            System.out.println("The weather is BAD 😩");
+        }
+    }
 }
