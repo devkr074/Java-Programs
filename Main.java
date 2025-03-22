1da511c471aaeb39e973ffe6b32f67a6c445463c
Dev Kumar <devkr074@gmail.com>
2025-03-23 00:38:36 +0530

    for loop in Java

---
+++ Main.java
@@ -0,0 +1,16 @@
+// for loop in Java
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("How many seconds to countdown from?: ");
+        int start = scanner.nextInt();
+        for(int i = start; i > 0; i--){
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+        System.out.println("HAPPY NEW YEAR!");
+        scanner.close();
+    }
+}
