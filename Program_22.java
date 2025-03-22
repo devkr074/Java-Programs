007ae5fb69f389536cef10de9c90bce0c6ed7ab8
Dev Kumar <devkr074@gmail.com>
2025-03-23 00:37:04 +0530

    Number Guessing Game in Java

--- Program_22.java
+++ Program_22.java
@@ -1,19 +1,32 @@
-// Positive, Negative, or Zero in Java
+// Number Guessing Game in Java
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Program_22 {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter an integer: ");
-    int num = sc.nextInt();
-    if (num > 0) {
-      System.out.println("The number is positive.");
-    } else if (num < 0) {
-      System.out.println("The number is negative.");
-    } else {
-      System.out.println("The number is zero.");
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int guess;
+        int attempts = 0;
+        int min = 1;
+        int max = 100;
+        int randomNumber = random.nextInt(min, max + 1);
+        System.out.println("Number Guessing Game");
+        System.out.printf("Guess a number between %d-%d\n", min, max);
+        do {
+            System.out.print("Enter a guess: ");
+            guess = scanner.nextInt();
+            attempts++;
+            if (guess < randomNumber) {
+                System.out.println("TOO LOW! Try again");
+            } else if (guess > randomNumber) {
+                System.out.println("TOO HIGH! Try again");
+            } else {
+                System.out.println("CORRECT! The number was " + randomNumber);
+                System.out.println("# of attempts: " + attempts);
+            }
+        } while (guess != randomNumber);
+        scanner.close();
     }
-    sc.close();
-  }
 }
