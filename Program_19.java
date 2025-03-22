95f33aabcd62f3ad4308944ec9287e9b9a62b5be
Dev Kumar <devkr074@gmail.com>
2025-03-22 15:21:36 +0530

    Simple Calculator in Java

--- Program_19.java
+++ Program_19.java
@@ -1,19 +1,42 @@
-// Reverse a Number in Java
+// Simple Calculator in Java
 
 import java.util.Scanner;
-
 public class Program_19 {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter a positive integer: ");
-    int num = sc.nextInt();
-    int reverse = 0;
-    while (num != 0) {
-      int digit = num % 10;
-      reverse = reverse * 10 + digit;
-      num /= 10;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double num1;
+        double num2;
+        char operator;
+        double result = 0;
+        boolean validOperation = true;
+        System.out.print("Enter the first number: ");
+        num1 = scanner.nextDouble();
+        System.out.print("Enter an operator (+, -, *, /, ^): ");
+        operator = scanner.next().charAt(0);
+        System.out.print("Enter the second number: ");
+        num2 = scanner.nextDouble();
+        switch(operator){
+            case '+' -> result = num1 + num2;
+            case '-' -> result = num1 - num2;
+            case '*' -> result = num1 * num2;
+            case '/' -> {
+                if(num2 == 0){
+                    System.out.println("Cannot divide by zero!");
+                    validOperation = false;
+                }
+                else{
+                    result = num1 / num2;
+                }
+            }
+            case '^' -> result = Math.pow(num1, num2);
+            default -> {
+                System.out.println("Invalid operator!");
+                validOperation = false;
+            }
+        }
+        if(validOperation){
+            System.out.println(result);
+        }
+        scanner.close();
     }
-    System.out.println("Reversed number: " + reverse);
-    sc.close();
-  }
 }
