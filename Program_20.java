bd5e36b24646dd321c6992b5232b83760ae03bbb
Dev Kumar <devkr074@gmail.com>
2025-03-22 15:22:53 +0530

    Logical Operator in Java

--- Program_20.java
+++ Program_20.java
@@ -1,22 +1,19 @@
-// Fibonacci Series in Java
-
-import java.util.Scanner;
+// Logical Operator in Java
 
 public class Program_20 {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter the number of terms: ");
-    int N = sc.nextInt();
-    int t1 = 0, t2 = 1;
-    int count = 1;
-    System.out.print("First " + N + " terms: ");
-    do {
-      System.out.print(t1 + " ");
-      int sum = t1 + t2;
-      t1 = t2;
-      t2 = sum;
-      count++;
-    } while (count <= N);
-    sc.close();
+      double temp = 35;
+      boolean isSunny = true;
+      if(temp <= 30 && temp >= 0 && isSunny){
+          System.out.println("The weather is GOOD 😀");
+          System.out.println("It is SUNNY outside ☀");
+      }
+      else if(temp <= 30 && temp >= 0 && !isSunny){
+          System.out.println("The weather is GOOD 😀");
+          System.out.println("It is CLOUDY outside ☁");
+      }
+      else if(temp > 30 || temp < 0){
+          System.out.println("The weather is BAD 😩");
+      }
   }
 }
