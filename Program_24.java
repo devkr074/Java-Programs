6a101e5dcccd5c22baa87912a65ac1b6aa0cee2e
Dev Kumar <devkr074@gmail.com>
2025-05-05 19:27:57 +0530

    break and continue in Java

--- Program_24.java
+++ Program_24.java
@@ -1,21 +1,12 @@
-// Largest of Three Numbers in Java
-
-import java.util.Scanner;
+// break and continue in Java
 
 public class Program_24 {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter three integers:");
-    int num1 = sc.nextInt();
-    int num2 = sc.nextInt();
-    int num3 = sc.nextInt();
-    if (num1 >= num2 && num1 >= num3) {
-      System.out.println("The largest number is " + num1);
-    } else if (num2 >= num1 && num2 >= num3) {
-      System.out.println("The largest number is " + num2);
-    } else {
-      System.out.println("The largest number is " + num3);
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+                break;
+            }
+            System.out.print(i + " ");
+        }
     }
-    sc.close();
-  }
 }
