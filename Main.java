6995a14729a5fefa2f2595036aa0d7ab210c3887
Dev Kumar <devkr074@gmail.com>
2025-05-06 19:05:16 +0530

    Scope of Variable in Java

---
+++ Main.java
@@ -0,0 +1,16 @@
+// Scope of Variable in Java
+
+public class Main {
+    static int x = 3;
+
+    public static void main(String[] args) {
+        int x = 1;
+        System.out.println(x);
+        doSomething();
+    }
+
+    static void doSomething() {
+        int x = 2;
+        System.out.println(x);
+    }
+}
