c98061404e015ca05de31d169ee04681c9a14c74
Dev Kumar <devkr074@gmail.com>
2025-05-10 19:44:07 +0530

    Object in Java

---
+++ Program_39.java
@@ -0,0 +1,14 @@
+// Object in Java
+
+public class Program_39 {
+    public static void main(String[] args) {
+        Car car = new Car();
+        System.out.println(car.make);
+        System.out.println(car.model);
+        System.out.println(car.year);
+        System.out.println(car.price);
+        System.out.println(car.isRunning);
+        car.drive();
+        car.brake();
+    }
+}
