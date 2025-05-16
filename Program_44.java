81ead92f0135041e0cacc6a13ea859328e01ff90
Dev Kumar <devkr074@gmail.com>
2025-05-16 11:49:59 +0530

    Inheritance in Java

---
+++ Program_44.java
@@ -0,0 +1,9 @@
+// Inheritance in Java
+
+public class Program_44 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Plant plant = new Plant();
+    }
+}
