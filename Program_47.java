35a074525b417591ce9de8dcb0db293266697c51
Dev Kumar <devkr074@gmail.com>
2025-05-19 13:35:38 +0530

    toString() in Java

---
+++ Program_47.java
@@ -0,0 +1,10 @@
+// toString() in Java
+
+public class Program_47 {
+    public static void main(String[] args) {
+        Car3 car1 = new Car3("Ford", "Mustang", 2025, "Red");
+        Car3 car2 = new Car3("Chevrolet", "Corvette", 2026, "Blue");
+        System.out.println(car1);
+        System.out.println(car2);
+    }
+}
