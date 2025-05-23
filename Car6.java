07f908bae2102a45eb91b9c6915cd72884221b32
Dev Kumar <devkr074@gmail.com>
2025-05-23 19:12:45 +0530

    Composition in Java

---
+++ Car6.java
@@ -0,0 +1,16 @@
+public class Car6 {
+    String model;
+    int year;
+    Engine engine;
+
+    Car6(String model, int year, String engineType) {
+        this.model = model;
+        this.year = year;
+        this.engine = new Engine(engineType);
+    }
+
+    void start() {
+        this.engine.start();
+        System.out.println("The " + this.model + " is running");
+    }
+}
