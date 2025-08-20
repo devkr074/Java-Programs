2eeda7422ec55be9de9bb438cc39fcc9d149a34d
Dev Kumar <devkr074@gmail.com>
2025-08-20 10:19:09 +0530

    Hello World!

--- Program_1.java
+++ Program_1.java
@@ -1,17 +1,7 @@
-// Message Printing and Comment in Java
+// Hello World!
 
 public class Program_1 {
     public static void main(String[] args) {
-        // This is single line comment
-        /*
-         * This
-         * is
-         * a
-         * multi-line
-         * comment
-         */
-        System.out.println("I like pizza!");
-        System.out.println("It's really good!");
-        System.out.println("Buy me pizza!");
+        System.out.println("Hello World!");
     }
 }
