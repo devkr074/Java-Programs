f515c73e3e950dadc30f420d3d76f5408ac18994
Dev Kumar <devkr074@gmail.com>
2025-08-20 10:23:07 +0530

    Printing favourite quote

--- Program_3.java
+++ Program_3.java
@@ -1,26 +1,7 @@
-// User Input in Java
-
-import java.util.Scanner;
+// Printing favourite quote
 
 public class Program_3 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
-        System.out.print("What is your gpa: ");
-        double gpa = scanner.nextDouble();
-        System.out.print("Are you a student? (true/false): ");
-        boolean isStudent = scanner.nextBoolean();
-        System.out.println("Hello " + name);
-        System.out.println("You are " + age + " years old");
-        System.out.println("Your gpa is: " + gpa);
-        if (isStudent) {
-            System.out.println("You are enrolled in classes");
-        } else {
-            System.out.println("You are NOT enrolled in classes");
-        }
-        scanner.close();
+        System.out.println("The only limit to our realization of tomorrow is our doubts of today.");
     }
 }
