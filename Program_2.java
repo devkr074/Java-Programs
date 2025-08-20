94e45889142024a9f197b98b94a4e2d9ef261af3
Dev Kumar <devkr074@gmail.com>
2025-08-20 10:20:46 +0530

    Printing Name, Age, and City

--- Program_2.java
+++ Program_2.java
@@ -1,45 +1,9 @@
-// Variables in Java
+// Printing Name, Age, and City
 
 public class Program_2 {
     public static void main(String[] args) {
-        int age = 21;
-        System.out.println(age);
-        int year = 2025;
-        System.out.println(year);
-        int quantity = 1;
-        System.out.println(quantity);
-        double price = 19.99;
-        System.out.println(price);
-        double gpa = 3.5;
-        System.out.println(gpa);
-        double temperature = -12.5;
-        System.out.println(temperature);
-        char grade = 'A';
-        System.out.println(grade);
-        char symbol = '!';
-        System.out.println(symbol);
-        char currency = '$';
-        System.out.println(currency);
-        boolean isStudent = true;
-        System.out.println(isStudent);
-        boolean forSale = false;
-        System.out.println(forSale);
-        boolean isOnline = true;
-        System.out.println(isOnline);
-        String name = "Bro Code";
-        System.out.println(name);
-        String food = "pizza";
-        System.out.println(food);
-        String email = "fake123@gmail.com";
-        System.out.println(email);
-        String car = "Mustang";
-        String color = "red";
-        System.out.println("Your choice is a " + color + " " + year + " " + car);
-        System.out.println("The price is: " + currency + price);
-        if (forSale) {
-            System.out.println("There is a " + car + " for sale");
-        } else {
-            System.out.println("The " + car + " is not for sale");
-        }
+        System.out.println("Name: Dev Kumar");
+        System.out.println("Age: 20");
+        System.out.println("City: Lucknow");
     }
 }
