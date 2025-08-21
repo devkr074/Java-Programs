68f46748be6ff148b84303b81b731786962a7f4e
Dev Kumar <devkr074@gmail.com>
2025-08-21 10:07:45 +0530

    Addition

--- Program_6.java
+++ Program_6.java
@@ -1,24 +1,15 @@
-// Shopping Cart in Java
+// Addition
 
 import java.util.Scanner;
 
 public class Program_6 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String item;
-        double price;
-        int quantity;
-        char currency = '$';
-        double total;
-        System.out.print("What item would you like to buy?: ");
-        item = scanner.nextLine();
-        System.out.print("What is the price for each?: ");
-        price = scanner.nextDouble();
-        System.out.print("How many would you like?: ");
-        quantity = scanner.nextInt();
-        total = price * quantity;
-        System.out.println("\nYou have bought " + quantity + " " + item + "/s");
-        System.out.println("Your total is " + currency + total);
-        scanner.close();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        System.out.println("Sum: " + (num1 + num2));
+        sc.close();
     }
 }
