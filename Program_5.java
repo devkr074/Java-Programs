a3ab7ec3d44d2fa0a3cc44f2fa7fdcacb3aa47a0
Dev Kumar <devkr074@gmail.com>
2025-08-21 10:05:55 +0530

    User Input

--- Program_5.java
+++ Program_5.java
@@ -1,15 +1,17 @@
-// Arithmetic Operation in Java
+// User Input
+
+import java.util.Scanner;
 
 public class Program_5 {
   public static void main(String[] args) {
-    int x = 10;
-    int y = 2;
-    int z;
-    z = x + y;
-    z = x - y;
-    z = x * y;
-    z = x / y;
-    z = x % y;
-    System.out.println(z);
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter your name: ");
+    String name = sc.nextLine();
+    System.out.print("Enter your age: ");
+    int age = sc.nextInt();
+    System.out.print("Enter your city: ");
+    String city = sc.nextLine();
+    System.out.println("Hello, " + name + "! You are " + age + " years old. You live in " + city + ".");
+    sc.close();
   }
 }
