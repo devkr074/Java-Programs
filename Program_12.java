0400e2ae445167ff18dda5894ef92a2b61119d35
Dev Kumar <devkr074@gmail.com>
2025-08-22 10:37:26 +0530

    Voting Eligibility

--- Program_12.java
+++ Program_12.java
@@ -1,27 +1,14 @@
-// Nested if Statement in Java
+// Voting Eligibility
+
+import java.util.Scanner;
 
 public class Program_12 {
     public static void main(String[] args) {
-        boolean isStudent = true;
-        boolean isSenior = true;
-        double price = 9.99;
-        if (isStudent) {
-            if (isSenior) {
-                System.out.println("You get a senior discount of 20%");
-                System.out.println("You get a student discount of 10%");
-                price *= 0.7;
-            } else {
-                System.out.println("You get a student discount of 10%");
-                price *= 0.9;
-            }
-        } else {
-            if (isSenior) {
-                System.out.println("You get a senior discount of 20%");
-                price *= 0.8;
-            } else {
-                price *= 1;
-            }
-        }
-        System.out.printf("The price of a ticket is: $%.2f", price);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        String eligibility = (age >= 18) ? "eligible" : "not eligible";
+        System.out.println("You are " + eligibility + " to vote.");
+        scanner.close();
     }
 }
