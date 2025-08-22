5a026dac74a7d102fe55b8a660bcac3bc09550af
Dev Kumar <devkr074@gmail.com>
2025-08-22 10:33:57 +0530

    Even or Odd

--- Program_10.java
+++ Program_10.java
@@ -1,34 +1,14 @@
-// printf() in Java
+// Even or Odd
+
+import java.util.Scanner;
 
 public class Program_10 {
   public static void main(String[] args) {
-    String name = "Spongebob";
-    char firstLetter = 'S';
-    int age = 30;
-    double height = 60.5;
-    boolean isEmployed = true;
-    System.out.printf("Hello %s\n", name);
-    System.out.printf("Your name starts with a %c\n", firstLetter);
-    System.out.printf("You are %d years old\n", age);
-    System.out.printf("You are %f inches tall\n", height);
-    System.out.printf("Employed: %b\n", isEmployed);
-    System.out.printf("%s is %d years old", name, age);
-    double price1 = 9.99;
-    double price2 = 100.15;
-    double price3 = -54.01;
-    System.out.printf("%.3f\n", price1);
-    System.out.printf("%.3f\n", price2);
-    System.out.printf("%.3f\n", price3);
-    System.out.printf("%f\n", price1);
-    System.out.printf("%f\n", price2);
-    System.out.printf("%f\n", price3);
-    int id1 = 1;
-    int id2 = 23;
-    int id3 = 456;
-    int id4 = 7890;
-    System.out.printf("id: %04d\n", id1);
-    System.out.printf("id: %04d\n", id2);
-    System.out.printf("id: %04d\n", id3);
-    System.out.printf("id: %04d\n", id4);
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int num = scanner.nextInt();
+    String result = (num % 2 == 0) ? "Even" : "Odd";
+    System.out.println(num + " is " + result + ".");
+    scanner.close();
   }
 }
