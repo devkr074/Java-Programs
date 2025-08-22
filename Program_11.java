b4b77edf10721eae3c87f8feaeee683842f33bee
Dev Kumar <devkr074@gmail.com>
2025-08-22 10:35:36 +0530

    Greater Number

--- Program_11.java
+++ Program_11.java
@@ -1,25 +1,16 @@
-// Compound Interest Calculator in Java
+// Greater Number
 
 import java.util.Scanner;
 
 public class Program_11 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double principal;
-        double rate;
-        int timesCompounded;
-        int years;
-        double amount;
-        System.out.print("Enter the principal amount: ");
-        principal = scanner.nextDouble();
-        System.out.print("Enter the interest rate (in %): ");
-        rate = scanner.nextDouble() / 100;
-        System.out.print("Enter the # of times compounded per year: ");
-        timesCompounded = scanner.nextInt();
-        System.out.print("Enter the # of years: ");
-        years = scanner.nextInt();
-        amount = principal * Math.pow(1 + rate / timesCompounded, timesCompounded * years);
-        System.out.printf("The amount after %d years is $%.2f", years, amount);
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+        int greater = (num1 > num2) ? num1 : num2;
+        System.out.println("Greater number is: " + greater);
         scanner.close();
     }
 }
