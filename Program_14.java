17b3d96ff840cbaa59b108df7f0931a7a12850e5
Dev Kumar <devkr074@gmail.com>
2025-08-22 10:43:46 +0530

    Bitwise AND, OR, XOR

--- Program_14.java
+++ Program_14.java
@@ -1,23 +1,20 @@
-// substring in Java
+// Bitwise AND, OR, XOR
 
 import java.util.Scanner;
 
 public class Program_14 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String email;
-        String username;
-        String domain;
-        System.out.print("Enter your email: ");
-        email = scanner.nextLine();
-        if (email.contains("@")) {
-            username = email.substring(0, email.indexOf("@"));
-            domain = email.substring(email.indexOf("@") + 1);
-            System.out.println(username);
-            System.out.println(domain);
-        } else {
-            System.out.println("Emails must contain @");
-        }
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+        int andResult = num1 & num2;
+        int orResult = num1 | num2;
+        int xorResult = num1 ^ num2;
+        System.out.println("Bitwise AND: " + andResult);
+        System.out.println("Bitwise OR: " + orResult);
+        System.out.println("Bitwise XOR: " + xorResult);
         scanner.close();
     }
 }
