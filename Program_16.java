dbe5d5bbe7605834c1279e2a084ead4d186c26cd
Dev Kumar <devkr074@gmail.com>
2025-08-23 15:30:24 +0530

    Largest among three numbers

--- Program_16.java
+++ Program_16.java
@@ -1,9 +1,23 @@
-// Ternary Operator in Java
+// Largest among three numbers
+
+import java.util.Scanner;
 
 public class Program_16 {
     public static void main(String[] args) {
-        int number = 10;
-        String evenOrOdd = (number % 2 == 0) ? "EVEN" : "ODD";
-        System.out.println(evenOrOdd);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+        System.out.print("Enter third number: ");
+        int num3 = scanner.nextInt();
+        if (num1 >= num2 && num1 >= num3) {
+            System.out.println("The largest number is: " + num1);
+        } else if (num2 >= num1 && num2 >= num3) {
+            System.out.println("The largest number is: " + num2);
+        } else {
+            System.out.println("The largest number is: " + num3);
+        }
+        scanner.close();
     }
 }
