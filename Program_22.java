de27a54372bf5ec59c8159e1264350343baa2edd
Dev Kumar <devkr074@gmail.com>
2025-08-24 15:08:35 +0530

    Sum of first 10 natural numbers

--- Program_22.java
+++ Program_22.java
@@ -1,32 +1,13 @@
-// Number Guessing Game in Java
+// Sum of first 10 natural numbers
 
-import java.util.Random;
-import java.util.Scanner;
-
-public class Program_22 {
-    public static void main(String[] args) {
-        Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
-        int guess;
-        int attempts = 0;
-        int min = 1;
-        int max = 100;
-        int randomNumber = random.nextInt(min, max + 1);
-        System.out.println("Number Guessing Game");
-        System.out.printf("Guess a number between %d-%d\n", min, max);
+class Program_22 {
+    public static void main(String args[]) {
+        int sum = 0;
+        int i = 1;
         do {
-            System.out.print("Enter a guess: ");
-            guess = scanner.nextInt();
-            attempts++;
-            if (guess < randomNumber) {
-                System.out.println("TOO LOW! Try again");
-            } else if (guess > randomNumber) {
-                System.out.println("TOO HIGH! Try again");
-            } else {
-                System.out.println("CORRECT! The number was " + randomNumber);
-                System.out.println("# of attempts: " + attempts);
-            }
-        } while (guess != randomNumber);
-        scanner.close();
+            sum += i;
+            i++;
+        } while (i <= 10);
+        System.out.println("Sum of first 10 natural numbers is: " + sum);
     }
 }
