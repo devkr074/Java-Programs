ef6081379038a6ef79bed6d7aca37607ef2a4c00
Dev Kumar <devkr074@gmail.com>
2025-08-24 15:05:15 +0530

    Printing numbers from 1 to 10

--- Program_20.java
+++ Program_20.java
@@ -1,17 +1,11 @@
-// Logical Operator in Java
+// Printing numbers from 1 to 10
 
 public class Program_20 {
     public static void main(String[] args) {
-        double temp = 35;
-        boolean isSunny = true;
-        if (temp <= 30 && temp >= 0 && isSunny) {
-            System.out.println("The weather is GOOD 😀");
-            System.out.println("It is SUNNY outside ☀");
-        } else if (temp <= 30 && temp >= 0 && !isSunny) {
-            System.out.println("The weather is GOOD 😀");
-            System.out.println("It is CLOUDY outside ☁");
-        } else if (temp > 30 || temp < 0) {
-            System.out.println("The weather is BAD 😩");
+        int number = 1;
+        while (number <= 10) {
+            System.out.println(number);
+            number++;
         }
     }
 }
