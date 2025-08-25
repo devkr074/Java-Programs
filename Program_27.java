0c3816d641c007faa5422aeb2e54333d04520544
Dev Kumar <devkr074@gmail.com>
2025-08-25 09:44:53 +0530

    Largest element of an Array

--- Program_27.java
+++ Program_27.java
@@ -1,20 +1,22 @@
-// Overloaded Method in Java
+// Largest element of an Array
+
+import java.util.Scanner;
 
 public class Program_27 {
     public static void main(String[] args) {
-        String pizza = bakePizza("flat-bread", "mozzarella", "pepperoni");
-        System.out.println(pizza);
-    }
-
-    static String bakePizza(String bread) {
-        return bread + " pizza";
-    }
-
-    static String bakePizza(String bread, String cheese) {
-        return cheese + " " + bread + " pizza";
-    }
-
-    static String bakePizza(String bread, String cheese, String topping) {
-        return topping + " " + cheese + " " + bread + " pizza";
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[5];
+        System.out.println("Enter 5 integers:");
+        for (int i = 0; i < 5; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        int largest = numbers[0];
+        for (int num : numbers) {
+            if (num > largest) {
+                largest = num;
+            }
+        }
+        System.out.println("The largest integer is: " + largest);
+        scanner.close();
     }
 }
