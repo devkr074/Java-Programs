5571ad3c0e04abc1d34df4a0a02ea4d38e9d5417
Dev Kumar <devkr074@gmail.com>
2025-08-26 11:57:08 +0530

    Data Types

---
+++ DataTypes.java
@@ -0,0 +1,17 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        // Primitive data types
+        int age = 20;
+        double height = 5.9;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        // String (Reference type)
+        String name = "Dev";
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " feet");
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
