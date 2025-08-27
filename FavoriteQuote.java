5a9d5e720f737947e6a4ed25e29d32126a834bb0
Dev Kumar <devkr074@gmail.com>
2025-08-27 10:11:19 +0530

    Favorite Quote

---
+++ FavoriteQuote.java
@@ -0,0 +1,5 @@
+public class FavoriteQuote {
+    public static void main(String[] args) {
+        System.out.println("The only limit to our realization of tomorrow is our doubts of today.");
+    }
+}
