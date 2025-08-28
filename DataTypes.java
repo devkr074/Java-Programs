e0e8de608ba8d6f7e11025c976db8d5509ad6db6
Dev Kumar <devkr074@gmail.com>
2025-08-28 10:09:46 +0530

    Data Types

---
+++ DataTypes.java
@@ -0,0 +1,20 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        byte b = 100;
+        System.out.println("byte value: " + b);
+        short s = 10000;
+        System.out.println("short value: " + s);
+        int i = 10;
+        System.out.println("int value: " + i);
+        long l = 100000L;
+        System.out.println("long value: " + l);
+        double d = 5.99;
+        System.out.println("double value: " + d);
+        float f = 5.99f;
+        System.out.println("float value: " + f);
+        char c = 'A';
+        System.out.println("char value: " + c);
+        boolean bool = true;
+        System.out.println("boolean value: " + bool);
+    }
+}
