e5f919480192dd56d5fa33ed2d1edf1ab6cd056c
Dev Kumar <devkr074@gmail.com>
2025-08-31 00:50:27 +0530

    Variables

---
+++ Program_4.java
@@ -0,0 +1,19 @@
+public class Program_4 {
+    public static void main(String[] args) {
+        String name = "John";
+        System.out.println(name);
+        int num1 = 15;
+        System.out.println(num1);
+        int num2;
+        num2 = 15;
+        System.out.println(num2);
+        int num3 = 15;
+        num3 = 20;
+        System.out.println(num3);
+        final int myNum = 15;
+        // myNum = 20;
+        float myFloatNum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+    }
+}
