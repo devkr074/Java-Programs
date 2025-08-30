97ac0c656a46f3610c7406f5e4a469e3e77d9fcb
Dev Kumar <devkr074@gmail.com>
2025-08-31 00:53:53 +0530

    Multiple Variables

---
+++ Program_6.java
@@ -0,0 +1,13 @@
+public class Program_6 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 50;
+        System.out.println(a + b + c);
+        int p = 5, q = 6, r = 50;
+        System.out.println(p + q + r);
+        int x, y, z;
+        x = y = z = 50;
+        System.out.println(x + y + z);
+    }
+}
