0bca310792d103d50480d69e7399508cb9213a2c
Dev Kumar <devkr074@gmail.com>
2025-08-31 00:52:14 +0530

    Print Variables

---
+++ Program_5.java
@@ -0,0 +1,13 @@
+public class Program_5 {
+    public static void main(String[] args) {
+        String name = "John";
+        System.out.println("Hello " + name);
+        String firstName = "John ";
+        String lastName = "Doe";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+        int x = 5;
+        int y = 6;
+        System.out.println(x + y);
+    }
+}
