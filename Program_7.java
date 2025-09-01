668f9cbbfc8445382f47094bf2e6d15ca988b663
Dev Kumar <devkr074@gmail.com>
2025-09-01 20:04:47 +0530

    Identifiers

---
+++ Program_7.java
@@ -0,0 +1,8 @@
+public class Program_7 {
+    public static void main(String[] args) {
+        int minutesPerHour = 60;
+        int m = 60;
+        System.out.println(minutesPerHour);
+        System.out.println(m);
+    }
+}
