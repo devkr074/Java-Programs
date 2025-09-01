8e23dd69f30556f1452ad239a56ab5f3ecb4e7b2
Dev Kumar <devkr074@gmail.com>
2025-09-01 20:07:40 +0530

    Real-Life Examples

---
+++ Program_9.java
@@ -0,0 +1,21 @@
+public class Program_9 {
+    public static void main(String[] args) {
+        String studentName = "John Doe";
+        int studentID = 15;
+        int studentAge = 23;
+        float studentFee = 75.25f;
+        char studentGrade = 'B';
+        System.out.println("Student name: " + studentName);
+        System.out.println("Student id: " + studentID);
+        System.out.println("Student age: " + studentAge);
+        System.out.println("Student fee: " + studentFee);
+        System.out.println("Student grade: " + studentGrade);
+        int length = 4;
+        int width = 6;
+        int area;
+        area = length * width;
+        System.out.println("Length is: " + length);
+        System.out.println("Width is: " + width);
+        System.out.println("Area of the rectangle is: " + area);
+    }
+}
