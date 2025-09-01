5667aab5aa93c8ef075c7d06212124db26261bb4
Dev Kumar <devkr074@gmail.com>
2025-09-01 20:05:27 +0530

    Constants (Final)

---
+++ Program_8.java
@@ -0,0 +1,7 @@
+public class Program_8 {
+    public static void main(String[] args) {
+        final int MINUTES_PER_HOUR = 60;
+        // MINUTES_PER_HOUR = 50;
+        System.out.println(MINUTES_PER_HOUR);
+    }
+}
