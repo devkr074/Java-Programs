136b4d91c8bbf5c5841c0b772fffc7129b0b91f1
Dev Kumar <devkr074@gmail.com>
2025-09-02 10:17:36 +0530

    Numbers

---
+++ Program_11.java
@@ -0,0 +1,20 @@
+public class Program_11 {
+    public static void main(String[] args) {
+        byte myByteNum = 100;
+        System.out.println(myByteNum);
+        short myShortNum = 5000;
+        System.out.println(myShortNum);
+        int myIntNum = 100000;
+        System.out.println(myIntNum);
+        long myLongNum = 15000000000L;
+        System.out.println(myLongNum);
+        float myFloatNum = 5.75f;
+        System.out.println(myFloatNum);
+        double myDoubleNum = 19.99d;
+        System.out.println(myDoubleNum);
+        float f1 = 35e3f;
+        double d1 = 12E4d;
+        System.out.println(f1);
+        System.out.println(d1);
+    }
+}
