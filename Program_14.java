8f5b7837bb31f9cf5bf488b6295728e7004965d9
Dev Kumar <devkr074@gmail.com>
2025-09-08 23:40:34 +0530

    Real-Life Example

---
+++ Program_14.java
@@ -0,0 +1,11 @@
+public class Program_14 {
+    public static void main(String[] args) {
+        int items = 50;
+        float costPerItem = 9.99f;
+        float totalCost = items * costPerItem;
+        char currency = '$';
+        System.out.println("Number of items: " + items);
+        System.out.println("Cost per item: " + costPerItem + currency);
+        System.out.println("Total cost = " + totalCost + currency);
+    }
+}
