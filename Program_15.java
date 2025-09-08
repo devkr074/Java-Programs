061ec335ab775be655fd57f56264db1cb17347cd
Dev Kumar <devkr074@gmail.com>
2025-09-08 23:43:52 +0530

    The var Keyword

---
+++ Program_15.java
@@ -0,0 +1,16 @@
+public class Program_15 {
+    public static void main(String[] args) {
+        var x = 5;
+        System.out.println(x);
+        var myNum = 5;
+        var myDouble = 9.98;
+        var myChar = 'D';
+        var myBoolean = true;
+        var myString = "Hello";
+        // var y;
+        var y = 5;
+        var z = 5;
+        z = 10;
+        // z = 9.99;
+    }
+}
