d61ccf70a6a100922756b0feb112d302d9fcd3ed
Dev Kumar <devkr074@gmail.com>
2025-09-10 10:05:36 +0530

    Numbers and Strings

---
+++ Program_20.java
@@ -0,0 +1,11 @@
+public class Program_20 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);
+        String x = "10";
+        String y = "20";
+        System.out.println(x + y);
+        System.out.println(x + b);
+    }
+}
