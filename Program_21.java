cac213850390302f08c32640a4b54dcae63d1a0d
Dev Kumar <devkr074@gmail.com>
2025-09-10 10:07:37 +0530

    Special Characters

---
+++ Program_21.java
@@ -0,0 +1,10 @@
+public class Program_21 {
+    public static void main(String[] args) {
+        String txt = "We are the so-called \"Vikings\" from the north.";
+        System.out.println(txt);
+        txt = "It\'s alright.";
+        System.out.println(txt);
+        txt = "The character \\ is called backslash.";
+        System.out.println(txt);
+    }
+}
