b338816ba034c14ecb48f69c84526f288574f3db
Dev Kumar <devkr074@gmail.com>
2025-09-11 09:46:43 +0530

    Booleans

---
+++ Program_23.java
@@ -0,0 +1,17 @@
+public class Program_23 {
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);
+        System.out.println(isFishTasty);
+        int x = 10;
+        int y = 9;
+        System.out.println(x > y);
+        System.out.println(10 > 9);
+        System.out.println(x == 10);
+        System.out.println(10 == 15);
+        int myAge = 25;
+        int votingAge = 18;
+        System.out.println(myAge >= votingAge);
+    }
+}
