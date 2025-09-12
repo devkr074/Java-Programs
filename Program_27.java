3f5018e8c77146691fcd9ff6c596fcf65165e151
Dev Kumar <devkr074@gmail.com>
2025-09-12 09:27:24 +0530

    Short Hand If...Else

---
+++ Program_27.java
@@ -0,0 +1,12 @@
+public class Program_27 {
+    public static void main(String[] args) {
+        int time = 20;
+        if (time < 18) {
+            System.out.println("Good day.");
+        } else {
+            System.out.println("Good evening.");
+        }
+        String result = (time < 18) ? "Good day." : "Good evening.";
+        System.out.println(result);
+    }
+}
