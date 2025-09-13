ea0712353dde49daebe0807570fa1942fe4dbe5b
Dev Kumar <devkr074@gmail.com>
2025-09-13 11:02:33 +0530

    Do/While Loop

---
+++ Program_33.java
@@ -0,0 +1,14 @@
+public class Program_33 {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 5);
+        i = 10;
+        do {
+            System.out.println("i is " + i);
+            i++;
+        } while (i < 5);
+    }
+}
