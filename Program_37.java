bb9ef31f8a954efcbea9b73b691a7e2dbc468b2c
Dev Kumar <devkr074@gmail.com>
2025-09-14 00:09:53 +0530

    For-Each Loop

---
+++ Program_37.java
@@ -0,0 +1,12 @@
+public class Program_37 {
+    public static void main(String[] args) {
+        String[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+        for (String car : cars) {
+            System.out.println(car);
+        }
+        int[] numbers = { 10, 20, 30, 40 };
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
