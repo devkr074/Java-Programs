1cfe7de95f84c4350b11e163e07fe4372604562b
Dev Kumar <devkr074@gmail.com>
2025-09-13 11:03:55 +0530

    Real-Life Examples

---
+++ Program_34.java
@@ -0,0 +1,19 @@
+public class Program_34 {
+    public static void main(String[] args) {
+        int countdown = 3;
+        while (countdown > 0) {
+            System.out.println(countdown);
+            countdown--;
+        }
+        System.out.println("Happy New Year!!");
+        int dice = 1;
+        while (dice <= 6) {
+            if (dice < 6) {
+                System.out.println("No Yatzy.");
+            } else {
+                System.out.println("Yatzy!");
+            }
+            dice = dice + 1;
+        }
+    }
+}
