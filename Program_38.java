b6ab980bef07b7db7a968a35d15ae0bc1c654b18
Dev Kumar <devkr074@gmail.com>
2025-09-14 00:13:51 +0530

    Real-Life Examples

---
+++ Program_38.java
@@ -0,0 +1,17 @@
+public class Program_38 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i += 10) {
+            System.out.println(i);
+        }
+        for (int i = 0; i <= 10; i = i + 2) {
+            System.out.println(i);
+        }
+        int number = 2;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+        for (int seat = 1; seat <= 5; seat++) {
+            System.out.println("Seat number: " + seat);
+        }
+    }
+}
