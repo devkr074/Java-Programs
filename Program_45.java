d578435370cd898bdd255580e4b930912048a929
Dev Kumar <devkr074@gmail.com>
2025-09-16 09:50:15 +0530

    Parameters

---
+++ Program_45.java
@@ -0,0 +1,20 @@
+public class Program_45 {
+    static void myMethod(String fname, int age) {
+        System.out.println(fname + " is " + age);
+    }
+
+    static void checkAge(int age) {
+        if (age < 18) {
+            System.out.println("Access denied - You are not old enough!");
+        } else {
+            System.out.println("Access granted - You are old enough!");
+        }
+    }
+
+    public static void main(String[] args) {
+        myMethod("Liam", 5);
+        myMethod("Jenny", 8);
+        myMethod("Anja", 31);
+        checkAge(20);
+    }
+}
