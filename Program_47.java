59084c63cb4500e58ec29702cec858e55ccc3014
Dev Kumar <devkr074@gmail.com>
2025-09-17 10:12:23 +0530

    Method Overloading

---
+++ Program_47.java
@@ -0,0 +1,28 @@
+public class Program_47 {
+    static int plusMethodInt(int x, int y) {
+        return x + y;
+    }
+
+    static double plusMethodDouble(double x, double y) {
+        return x + y;
+    }
+
+    static int plusMethod(int x, int y) {
+        return x + y;
+    }
+
+    static double plusMethod(double x, double y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        int myNum1 = plusMethodInt(8, 5);
+        double myNum2 = plusMethodDouble(4.3, 6.26);
+        System.out.println("int: " + myNum1);
+        System.out.println("double: " + myNum2);
+        myNum1 = plusMethod(8, 5);
+        myNum2 = plusMethod(4.3, 6.26);
+        System.out.println("int: " + myNum1);
+        System.out.println("double: " + myNum2);
+    }
+}
