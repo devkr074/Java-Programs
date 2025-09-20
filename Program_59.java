23f848ac6aac92b3a515f31931b32864ce6ac4e9
Dev Kumar <devkr074@gmail.com>
2025-09-20 11:01:52 +0530

    Inheritance

---
+++ Program_59.java
@@ -0,0 +1,17 @@
+class Program_59 extends Vehicle {
+    private String modelName = "Mustang";
+
+    public static void main(String[] args) {
+        Program_59 myCar = new Program_59();
+        myCar.honk();
+        System.out.println(myCar.brand + " " + myCar.modelName);
+    }
+}
+
+class Vehicle {
+    protected String brand = "Ford";
+
+    public void honk() {
+        System.out.println("Tuut, tuut!");
+    }
+}
