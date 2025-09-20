4fc9fcc2c3ea12c0fbef14e71d28a08a1f6ea414
Dev Kumar <devkr074@gmail.com>
2025-09-21 01:58:09 +0530

    Inner Classes

---
+++ Program_62.java
@@ -0,0 +1,15 @@
+public class Program_62 {
+  public static void main(String[] args) {
+    OuterClass myOuter = new OuterClass();
+    OuterClass.InnerClass myInner = myOuter.new InnerClass();
+    System.out.println(myInner.y + myOuter.x);
+  }
+}
+
+class OuterClass {
+  int x = 10;
+
+  class InnerClass {
+    int y = 5;
+  }
+}
