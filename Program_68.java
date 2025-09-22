dfb97884f46ddd85329bb2901745074bf6dfbb59
Dev Kumar <devkr074@gmail.com>
2025-09-22 15:53:23 +0530

    User Input

---
+++ Program_68.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Program_68 {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter name, age and salary:");
+        String name = myObj.nextLine();
+        int age = myObj.nextInt();
+        double salary = myObj.nextDouble();
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        myObj.close();
+    }
+}
