5f0dd4fbccf3b08fa0fea98faa700e27f99cde8b
Dev Kumar <devkr074@gmail.com>
2025-09-23 09:55:29 +0530

    Debugging

---
+++ Program_71.java
@@ -0,0 +1,16 @@
+public class Program_71 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 0;
+        System.out.println("Before division");
+        int result = x / y;
+        System.out.println("Result: " + result);
+        int age = 17;
+        System.out.println("Age: " + age);
+        if (age >= 18) {
+            System.out.println("Access granted");
+        } else {
+            System.out.println("Access denied");
+        }
+    }
+}
