b9450e95e512f88b290aeadbaee1ffc863a54045
Dev Kumar <devkr074@gmail.com>
2025-09-26 10:15:00 +0530

    LinkedList

---
+++ Program_85.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Program_85 {
+    public static void main(String[] args) {
+        LinkedList<String> cars = new LinkedList<String>();
+        var cars2 = new LinkedList<String>();
+        List<String> cars3 = new LinkedList<>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
+    }
+}
