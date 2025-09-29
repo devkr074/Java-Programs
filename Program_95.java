cda9522e6e026033dc660109622771d991b32807
Dev Kumar <devkr074@gmail.com>
2025-09-30 01:41:43 +0530

    Wrapper Classes

---
+++ Program_95.java
@@ -0,0 +1,15 @@
+public class Program_95 {
+    public static void main(String[] args) {
+        Integer myInt = 5;
+        Double myDouble = 5.99;
+        Character myChar = 'A';
+        String myString = myInt.toString();
+        System.out.println(myInt);
+        System.out.println(myDouble);
+        System.out.println(myChar);
+        System.out.println(myInt.intValue());
+        System.out.println(myDouble.doubleValue());
+        System.out.println(myChar.charValue());
+        System.out.println(myString.length());
+    }
+}
