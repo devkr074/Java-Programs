b56136cf18390221386161a729791d3b1ed309b4
Dev Kumar <devkr074@gmail.com>
2025-10-05 00:04:06 +0530

    Celsius to Fahrenheit

---
+++ Program_117.java
@@ -0,0 +1,7 @@
+public class Program_117 {
+    public static void main(String[] args) {
+        double celsius = 23.5;
+        double fahrenheit = celsius * 9 / 5 + 32;
+        System.out.println(celsius + "C = " + fahrenheit + "F");
+    }
+}
