02174aaadb292bc74d27c0e82bfa7d4feb3293cd
Dev Kumar <devkr074@gmail.com>
2025-10-05 00:03:35 +0530

    Area of Rectangle

---
+++ Program_116.java
@@ -0,0 +1,8 @@
+public class Program_116 {
+    public static void main(String[] args) {
+        int length = 5;
+        int width = 2;
+        int area = length * width;
+        System.out.println("Area of rectangle: " + area);
+    }
+}
