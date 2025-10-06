0f94e8314e34e10ec5aae96ab5db9d1651f6772c
Dev Kumar <devkr074@gmail.com>
2025-10-06 09:27:33 +0530

    Random Number

---
+++ Program_120.java
@@ -0,0 +1,7 @@
+public class Program_120 {
+    public static void main(String[] args) {
+        Math.random();
+        int randomNum = (int) (Math.random() * 101);
+        System.out.println(randomNum);
+    }
+}
