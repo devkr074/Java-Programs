fca4d0bbb1ce60b1c5219dabb01f66e8ea1e9f64
Dev Kumar <devkr074@gmail.com>
2025-10-07 09:41:13 +0530

    Reverse a String

---
+++ Program_125.java
@@ -0,0 +1,10 @@
+public class Program_125 {
+    public static void main(String[] args) {
+        String originalStr = "Hello";
+        String reversedStr = "";
+        for (int i = 0; i < originalStr.length(); i++) {
+            reversedStr = originalStr.charAt(i) + reversedStr;
+        }
+        System.out.println("Reversed string: " + reversedStr);
+    }
+}
