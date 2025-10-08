822d47b8c700f7b1d90e1e108c67f5f2a4f424cd
Dev Kumar <devkr074@gmail.com>
2025-10-08 09:32:38 +0530

    Convert String to Array

---
+++ Program_128.java
@@ -0,0 +1,9 @@
+public class Program_128 {
+    public static void main(String[] args) {
+        String myStr = "Hello";
+        char[] myArray = myStr.toCharArray();
+        for (char i : myArray) {
+            System.out.println(i);
+        }
+    }
+}
