363917cdd83fbbdf45e9ee0045e0b9cb3bbeb91d
Dev Kumar <devkr074@gmail.com>
2025-10-09 09:20:31 +0530

    Find Array Average

---
+++ Program_132.java
@@ -0,0 +1,12 @@
+public class Program_132 {
+    public static void main(String[] args) {
+        int ages[] = { 20, 22, 18, 35, 48, 26, 87, 70 };
+        float avg, sum = 0;
+        int length = ages.length;
+        for (int age : ages) {
+            sum += age;
+        }
+        avg = sum / length;
+        System.out.println("The average age is: " + avg);
+    }
+}
